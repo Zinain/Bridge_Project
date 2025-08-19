70e420c99028a791b87728ea9dc63c1c9c9e7aa4
Zinain <ilzarik811@gmail.com>
2025-08-19 16:11:38 +1000

    Working motor function

--- bridge_api/src/main.rs
+++ bridge_api/src/main.rs
@@ -1,18 +1,28 @@
-use esp_idf_hal::prelude::*;
-use esp_idf_hal::gpio::{PinDriver, Output};
-use std::thread::sleep;
-use std::time::Duration;
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_sys as _; // Bindings to ESP-IDF
 
 fn main() -> anyhow::Result<()> {
-    esp_idf_sys::link_patches(); // required for ESP-IDF runtime
+    let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
 
-    let peripherals = Peripherals::take().unwrap();
-    let mut led = PinDriver::output(peripherals.pins.gpio48)?;
+    // On many boards, LED is on GPIO2 — change if yours is different
+    let mut pin1 = PinDriver::output(peripherals.pins.gpio4)?;
+    let mut pin2 = PinDriver::output(peripherals.pins.gpio5)?;
+    let mut pin3 = PinDriver::output(peripherals.pins.gpio10)?;
+    let mut pin4 = PinDriver::output(peripherals.pins.gpio11)?;
 
     loop {
-        led.set_high()?;
-        sleep(Duration::from_millis(500));
-        led.set_low()?;
-        sleep(Duration::from_millis(500));
+        pin4.set_high()?;           // LED on
+        pin2.set_low()?;
+        FreeRtos::delay_ms(5);  // 1 second
+        pin3.set_high()?;
+        pin1.set_low()?;
+        FreeRtos::delay_ms(5);
+        pin2.set_high()?;           // LED on
+        pin4.set_low()?;
+        FreeRtos::delay_ms(5);  // 1 second
+        pin1.set_high()?;
+        pin3.set_low()?;
+        FreeRtos::delay_ms(5);
     }
 }
