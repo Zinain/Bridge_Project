95ba72a757d19595c2bd2cf45f3fa4e63ef5791c
Ilzar <ailzar@bk.ru>
2025-09-02 12:16:03 +1000

    Working code base for ESP32

--- BRCP/BCPv1.1/BCPv1.1.ino
+++ BRCP/BCPv1.1/BCPv1.1.ino
@@ -17,21 +17,83 @@ WiFiServer server(80);
 String header;
 
 // Auxiliar variables to store the current output state
-String output26State = "off";
-String output27State = "off";
+//Red LED Variables current state
+String outputRED = "off";
+//Green LED Variables current state
+String outputGREEN = "off";
+//Motor pins current state
+String Brstat = "closed";
+//Override on/off state
+String overrided = "off";
+
 
 // Assign output variables to GPIO pins
-const int output26 = 26;
-const int output27 = 27;
+//Red LED pins
+const int outputRED1 = 2;
+const int outputRED2 = 4;
+const int outputRED4 = 5;
+const int outputRED3 = 6;
+//Green LED pins
+const int outputGREEN1 = 7;
+const int outputGREEN2 = 15;
+const int outputGREEN3 = 16;
+const int outputGREEN4 = 17;
+//Motor pins
+const int motor1pin1 = 18;
+const int motor1pin2 = 8;
+
+void GlightsOn(){
+
+}
+
+void RlightsOn(){
+
+}
+
+void GlightsOff(){
+
+}
+
+void RlightsOff(){
+
+}
+
+void motorF(){
+
+}
+
+void motorB(){
+
+}
+
+void motorS(){
+
+}
 
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
-  pinMode(output26, OUTPUT);
-  pinMode(output27, OUTPUT);
+  //RED pins
+  pinMode(outputRED1, OUTPUT);
+  pinMode(outputRED2, OUTPUT);
+  pinMode(outputRED3, OUTPUT);
+  pinMode(outputRED4, OUTPUT);
+  //GREEn pins
+  pinMode(outputGREEN1, OUTPUT);
+  pinMode(outputGREEN2, OUTPUT);
+  pinMode(outputGREEN3, OUTPUT);
+  pinMode(outputGREEN4, OUTPUT);
   // Set outputs to LOW
-  digitalWrite(output26, LOW);
-  digitalWrite(output27, LOW);
+  digitalWrite(outputRED1, LOW);
+  digitalWrite(outputRED2, LOW);
+  digitalWrite(outputRED3, LOW);
+  digitalWrite(outputRED4, LOW);
+
+  digitalWrite(outputGREEN1, LOW);
+  digitalWrite(outputGREEN2, LOW);
+  digitalWrite(outputGREEN3, LOW);
+  digitalWrite(outputGREEN4, LOW);
+  
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Setting AP (Access Point)…");
@@ -68,22 +130,50 @@ void loop(){
             client.println();
             
             // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              output26State = "on";
-              digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
+            if (header.indexOf("GET /red/on") >= 0) {
+              Serial.println("RED on");
+              outputRED = "on";
+              RlightsOn();
+            } else if (header.indexOf("GET /red/off") >= 0) {
+              Serial.println("RED off");
+              outputRED = "off";
+              RlightsOff();
+            } else if (header.indexOf("GET /green/on") >= 0) {
+              Serial.println("GREEN on");
+              outputGREEN = "on";
+              GlightsOn();
+            } else if (header.indexOf("GET /green/off") >= 0) {
+              Serial.println("GREEN off");
+              outputGREEN = "off";
+              GlightsOff();
+            } else if (header.indexOf("GET /all/on" ) >= 0) {
+              outputGREEN = "on";
+              outputRED = "on";
+              GlightsOn();
+              RlightsOn();
+            } else if (header.indexOf("GET /all/off" ) >= 0) {
+              outputGREEN = "off";
+              outputRED = "off";
+              GlightsOff();
+              RlightsOff();
+            } else if (header.indexOf("GET /open/off" ) >= 0) {
+              Brstat = "open";
+              outputRED = "on";
+              outputGREEN = "off";
+              GlightsOff();
+              RlightsOn();
+              motorF();
+              delay(1);
+              motorS();
+            } else if (header.indexOf("GET /close/off" ) >= 0) {
+              Brstat = "closed";
+              outputRED = "off";
+              outputGREEN = "on";
+              GlightsOn();
+              RlightsOff();
+              motorB();
+              delay(1);
+              motorS();
             }
             
             // Display the HTML web page
@@ -101,21 +191,33 @@ void loop(){
             client.println("<body><h1>ESP32 Web Server</h1>");
             
             // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            client.println("<p>RED - State " + outputRED + "</p>");
             // If the output26State is off, it displays the ON button       
-            if (output26State=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            if (outputRED=="off") {
+              client.println("<p><a href=\"/red/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+              client.println("<p><a href=\"/red/off\"><button class=\"button button2\">OFF</button></a></p>");
             } 
                
             // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            client.println("<p>GREEN - State " + outputGREEN + "</p>");
             // If the output27State is off, it displays the ON button       
-            if (output27State=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            if (outputGREEN=="off") {
+              client.println("<p><a href=\"/green/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+              client.println("<p><a href=\"/green/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            if(outputRED == "off" && outputGREEN == "off"){
+              client.println("<p><a href=\"/all/on\"><button class=\"button\">ALL ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/all/off\"><button class=\"button button2\">ALL OFF</button></a></p>");
+            }
+
+            if(Brstat == "closed"){
+              client.println("<p><a href=\"/open/on\"><button class=\"button\">OPEN BR</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/close/off\"><button class=\"button button2\">CLOSE BR</button></a></p>");
             }
             client.println("</body></html>");
             
@@ -130,6 +232,7 @@ void loop(){
           currentLine += c;      // add it to the end of the currentLine
         }
       }
+      delay(1);
     }
     // Clear the header variable
     header = "";
